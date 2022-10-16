32526a0c34c49ed0d1695c1169292be7264c2ea7
mandarpawar123 <44388158+mandarpawar123@users.noreply.github.com>
2022-10-16 22:36:27 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// header scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("header-scrolled");
+        
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+//nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapes=document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click",function(){
+        navCollapes.classList.remove("show");
+    })
+})  
